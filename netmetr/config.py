52d56f74c66b113dcc26de49dd43fcc5ec13dcae
Martin Prudek <martin.prudek@nic.cz>
2020-10-01 12:33:35 +0200

    config: Move uci config to a separate file

--- netmetr/config.py
+++ netmetr/config.py
@@ -1,80 +1,25 @@
-import euci
-
-from .exceptions import ConfigError
-from .logging import logger
+import random
 
 
-CONFIG = "netmetr"
-SETTINGS_SECTION = "settings"
-UCI_MANDATORY_OPTIONS = {
-    "autostart_enabled", "hours_to_run", "autostart_delay",
-    "max_history_logs", "control_server", "protocol_mode"
+try:
+    from .uci_config import Config
+except ImportError as e:
+    if str(e) == "No module named 'euci'":
+        Config = dict
+    else:
+        raise e
+
+DEFAULT_CONFIG = {
+    "autostart_enabled": False,
+    "autostart_delay": random.randint(0, 3600),
+    "control_server": "control.netmetr.cz",
+    "max_history_logs": 10,
+    "uuid": None,
+    "hours_to_run": (random.randint(0, 23),),
+    "sync_code": None,
+    "protocol_mode": "prefer_6"
 }
 
 
-class Config(dict):
-    def __init__(self, def_config):
-        self.uci = euci.EUci()
-        try:
-            config = self.uci.get(CONFIG)
-        except euci.UciExceptionNotFound as e:
-            raise ConfigError("Netmetr config not found") from e
-
-        settings = config.get(SETTINGS_SECTION)
-        if settings is None:
-            raise ConfigError("'{}' section missing in config".format(
-                SETTINGS_SECTION
-            ))
-        super().update(settings)
-
-        self._fill_missing_options(def_config)
-
-    def _fill_missing_options(self, def_config):
-        need_to_commit = False
-        for option, value in def_config.items():
-            if option not in self:
-                if option in UCI_MANDATORY_OPTIONS:
-                    log_option_set(option, value)
-                    self._uci_set(option, value)
-                    need_to_commit = True
-                else:
-                    log_option_used(option, value)
-
-                if need_to_commit:
-                    self.uci.commit(CONFIG)
-
-                super().__setitem__(option, value)
-
-    def __setitem__(self, option, value):
-        if self.get(option) == value:
-            return
-
-        self._uci_set(option, value)
-        self.uci.commit(CONFIG, SETTINGS_SECTION, option)
-        super().__setitem__(option, value)
-
-    def __delitem__(self, option):
-        try:
-            super().__delitem__(option)
-        except KeyError:
-            logger.debug("Failed to delete config option '{}'".format(option))
-        self.uci.delete(CONFIG, SETTINGS_SECTION, option)
-        self.uci.commit(CONFIG, SETTINGS_SECTION, option)
-
-    def _uci_set(self, option, value):
-        try:
-            self.uci.set(CONFIG, SETTINGS_SECTION, option, value)
-        except euci.UciException as e:
-            raise ConfigError(
-                "Failed to assign value '{}' to option '{}' in config".format(
-                    value, option
-                )
-            ) from e
-
-
-def log_option_set(option, value):
-    logger.warning("Config option '{}' not found, setting to '{}'".format(option, value))
-
-
-def log_option_used(option, value):
-    logger.warning("Config option '{}' not found, using '{}'".format(option, value))
+def make_default_config():
+    return Config(DEFAULT_CONFIG)
---
+++ netmetr/uci_config.py
@@ -0,0 +1,81 @@
+import euci
+
+from .exceptions import ConfigError
+from .logging import logger
+
+
+CONFIG = "netmetr"
+SETTINGS_SECTION = "settings"
+UCI_MANDATORY_OPTIONS = {
+    "autostart_enabled", "hours_to_run", "autostart_delay",
+    "max_history_logs", "control_server", "protocol_mode"
+}
+
+
+class Config(dict):
+    def __init__(self, def_config):
+        super().__init__()
+        self.uci = euci.EUci()
+        try:
+            config = self.uci.get(CONFIG)
+        except euci.UciExceptionNotFound as e:
+            raise ConfigError("Netmetr config not found") from e
+
+        settings = config.get(SETTINGS_SECTION)
+        if settings is None:
+            raise ConfigError("'{}' section missing in config".format(
+                SETTINGS_SECTION
+            ))
+        super().update(settings)
+
+        self._fill_missing_options(def_config)
+
+    def _fill_missing_options(self, def_config):
+        need_to_commit = False
+        for option, value in def_config.items():
+            if option not in self:
+                if option in UCI_MANDATORY_OPTIONS:
+                    log_option_set(option, value)
+                    self._uci_set(option, value)
+                    need_to_commit = True
+                else:
+                    log_option_used(option, value)
+
+                if need_to_commit:
+                    self.uci.commit(CONFIG)
+
+                super().__setitem__(option, value)
+
+    def __setitem__(self, option, value):
+        if self.get(option) == value:
+            return
+
+        self._uci_set(option, value)
+        self.uci.commit(CONFIG, SETTINGS_SECTION, option)
+        super().__setitem__(option, value)
+
+    def __delitem__(self, option):
+        try:
+            super().__delitem__(option)
+        except KeyError:
+            logger.debug("Failed to delete config option '{}'".format(option))
+        self.uci.delete(CONFIG, SETTINGS_SECTION, option)
+        self.uci.commit(CONFIG, SETTINGS_SECTION, option)
+
+    def _uci_set(self, option, value):
+        try:
+            self.uci.set(CONFIG, SETTINGS_SECTION, option, value)
+        except euci.UciException as e:
+            raise ConfigError(
+                "Failed to assign value '{}' to option '{}' in config".format(
+                    value, option
+                )
+            ) from e
+
+
+def log_option_set(option, value):
+    logger.warning("Config option '{}' not found, setting to '{}'".format(option, value))
+
+
+def log_option_used(option, value):
+    logger.warning("Config option '{}' not found, using '{}'".format(option, value))
